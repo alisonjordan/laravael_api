ffdff81fb58beeee70b7e44d735ea0b7e69ddf49
unknown <alisonjor@gmail.com>
2023-11-21 15:05:21 -0300

    spa registering users and routes protection

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use App\Http\Controllers\Auth\LoginController;
+use App\Http\Controllers\Auth\LogoutController;
+use App\Http\Controllers\Auth\RegisterController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -20,5 +22,7 @@ Route::get('/', function () {
 
 Route::prefix('auth')->group(function(){
 
-    Route::post('/login', LoginController::class);
+    Route::post('/login', LoginController::class)->middleware('guest');
+    Route::post('/logout', LogoutController::class)->middleware('auth');
+    Route::post('/register', RegisterController::class)->middleware('guest');
 });
