5f089560fa1a221c7d1915870b5ceea3ca1e9434
unknown <alisonjor@gmail.com>
2023-11-27 15:26:24 -0300

    inserting a new task

--- todoapp/src/components/tasks/Tasks.vue
+++ todoapp/src/components/tasks/Tasks.vue
@@ -1,6 +1,6 @@
 <template>
 
-<div class="card mt-2" v-if="tasks.length">
+<div class="card mt-2" v-if="tasks.length && show">
                         <ul class="list-group list-group-flush">
                             <Task v-for="task in tasks" :task="task" :key="task.id"/>
                         </ul>
@@ -16,7 +16,11 @@ import Task from "./Task.vue"
 
 
 defineProps({
-tasks: Array
+    tasks: Array,
+    show: {
+        type: Boolean,
+        default: true
+    }
 
 })
 
--- todoapp/src/pages/TasksPage.vue
+++ todoapp/src/pages/TasksPage.vue
@@ -3,19 +3,22 @@
         <div class="container">
             <div class="row">
                 <div class="col-md-8 offset-md-2">
-                    <!-- Add new Task -->
-                    <div class="relative">
-                        <input type="text" class="form-control form-control-lg padding-right-lg"
-                            placeholder="+ Add new task. Press enter to save." />
-                    </div>
+                    <NewTask @added="handleAddedTask" />
+
                     <!-- List of uncompleted tasks -->
                     <Tasks :tasks="uncompletedTasks" />
 
                     <!-- show toggle buttom -->
-
+                    <div class="text-center my-3" v-show="showToggleCompletedBtn">
+                        <button class="btn btn-sm btn-secondary" 
+                            @click="showCompletedTasks = !showCompletedTasks">
+                            <span v-if="!showCompletedTasks">Show completed</span>
+                            <span v-else>Hide completed</span>
+                        </button>
+                    </div>
 
                     <!-- List of completed tasks -->
-                    <Tasks :tasks="completedTasks" />
+                    <Tasks :tasks="completedTasks" :show="completedTasksIsVisible && showCompletedTasks" />
 
                 </div>
             </div>
@@ -30,7 +33,8 @@
 import Tasks from "../components/tasks/Tasks.vue"
 
 import { computed, onMounted, ref } from "vue";
-import {allTasks} from "../http/task-api";
+import { allTasks, createTask } from "../http/task-api";
+import NewTask from "../components/tasks/NewTask.vue";
 const tasks = ref([])
 
 onMounted(async() => {
@@ -40,5 +44,19 @@ tasks.value = data.data
 
 const uncompletedTasks = computed(()=>tasks.value.filter(task=>!task.is_completed))
 const completedTasks = computed(()=>tasks.value.filter(task=>task.is_completed))
+
+const showToggleCompletedBtn = computed(
+    () => uncompletedTasks.value.length > 0 && completedTasks.value.length > 0
+)
+const completedTasksIsVisible = computed(
+    () => uncompletedTasks.value.length === 0 || completedTasks.value.length > 0
+)
+const showCompletedTasks = ref(false)
+
+
+const handleAddedTask = async (newTask) => {
+    const { data: createdTask } = await createTask(newTask)
+    tasks.value.unshift(createdTask.data)
+}
 
 </script>
