187fa399f1bd91eb9f0896eac34037e6347f586f
unknown <alisonjor@gmail.com>
2023-11-27 14:36:25 -0300

    using computed properties

--- todoapp/src/pages/TasksPage.vue
+++ todoapp/src/pages/TasksPage.vue
@@ -8,12 +8,15 @@
                         <input type="text" class="form-control form-control-lg padding-right-lg"
                             placeholder="+ Add new task. Press enter to save." />
                     </div>
-                    <!-- List of tasks -->
-                    <div class="card mt-2">
-                        <ul class="list-group list-group-flush">
-                            <Task v-for="task in tasks" :task="task" :key="task.id"/>
-                        </ul>
-                    </div>
+                    <!-- List of uncompleted tasks -->
+                    <Tasks :tasks="uncompletedTasks" />
+
+                    <!-- show toggle buttom -->
+
+
+                    <!-- List of completed tasks -->
+                    <Tasks :tasks="completedTasks" />
+
                 </div>
             </div>
         </div>
@@ -24,9 +27,9 @@
 
 <script setup>
 
-import Task from "../components/tasks/Task.vue"
+import Tasks from "../components/tasks/Tasks.vue"
 
-import { onMounted, ref } from "vue";
+import { computed, onMounted, ref } from "vue";
 import {allTasks} from "../http/task-api";
 const tasks = ref([])
 
@@ -34,5 +37,8 @@ onMounted(async() => {
 const {data} = await allTasks()
 tasks.value = data.data
 })
+
+const uncompletedTasks = computed(()=>tasks.value.filter(task=>!task.is_completed))
+const completedTasks = computed(()=>tasks.value.filter(task=>task.is_completed))
 
 </script>
